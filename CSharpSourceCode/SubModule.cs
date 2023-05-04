6fb499804ba52508ac9aa7cd8a56baca64b2ef40
Linus Tiemann <linus-tiemann@live.de>
2023-05-04 22:48:47 +0200

    first working approach

--- CSharpSourceCode/SubModule.cs
+++ CSharpSourceCode/SubModule.cs
@@ -1,16 +1,21 @@
 ﻿using System.Reflection;
 using TaleWorlds.CampaignSystem;
+using TaleWorlds.Library;
 using TaleWorlds.MountAndBlade;
 
 namespace DADG_Core
 {
     public class SubModule : MBSubModuleBase
     {
+        
+        protected override void OnBeforeInitialModuleScreenSetAsRoot()
+        {
+            InformationManager.DisplayMessage(new InformationMessage("DADG loaded", new TaleWorlds.Library.Color(134, 114, 250)));
+        }
         protected override void OnSubModuleLoad()
         {
             CampaignTime startTime = CampaignTime.Years(1471)+CampaignTime.Weeks(2)+CampaignTime.Days(3);
             typeof(CampaignData).GetField("CampaignStartTime",BindingFlags.Static|BindingFlags.Public)?.SetValue(null,startTime);
-            
         }
 
         
