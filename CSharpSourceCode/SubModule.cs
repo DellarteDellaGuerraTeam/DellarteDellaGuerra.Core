d9326d47495c83da40875451d363eadf417de99f
Linus Tiemann <linus-tiemann@live.de>
2023-05-04 20:22:42 +0200

    harmony

---
+++ CSharpSourceCode/SubModule.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.MountAndBlade;
+
+namespace DADG_Core
+{
+    public class SubModule : MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad()
+        {
+            CampaignTime startTime = CampaignTime.Years(1471)+CampaignTime.Weeks(2)+CampaignTime.Days(3);
+            typeof(CampaignData).GetField("CampaignStartTime",BindingFlags.Static|BindingFlags.Public)?.SetValue(null,startTime);
+            
+        }
+
+        
+    }
+}
