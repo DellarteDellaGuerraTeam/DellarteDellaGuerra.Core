72b5db99859cd3e811f189986be86932ed4ad0a7
Linus Tiemann <linus-tiemann@live.de>
2023-05-04 23:29:57 +0200

    adding harmony

--- CSharpSourceCode/SubModule.cs
+++ CSharpSourceCode/SubModule.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using HarmonyLib;
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.Library;
 using TaleWorlds.MountAndBlade;
@@ -7,7 +8,7 @@ namespace DADG_Core
 {
     public class SubModule : MBSubModuleBase
     {
-        
+        public static Harmony HarmonyInstance { get; private set; }
         protected override void OnBeforeInitialModuleScreenSetAsRoot()
         {
             InformationManager.DisplayMessage(new InformationMessage("DADG loaded", new TaleWorlds.Library.Color(134, 114, 250)));
@@ -16,6 +17,9 @@ namespace DADG_Core
         {
             CampaignTime startTime = CampaignTime.Years(1471)+CampaignTime.Weeks(2)+CampaignTime.Days(3);
             typeof(CampaignData).GetField("CampaignStartTime",BindingFlags.Static|BindingFlags.Public)?.SetValue(null,startTime);
+            
+            HarmonyInstance = new Harmony("mod.harmony.dellartedellaguerra");
+            HarmonyInstance.PatchAll();
         }
 
         
