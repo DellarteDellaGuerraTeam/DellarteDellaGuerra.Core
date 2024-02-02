0a93be1205f68705e40313af261b41004af7063e
Joël Forward <jforward.dev@gmail.com>
2024-02-02 03:01:45 +0100

    feat: print remaining compiling shaders to the user (#18) * create dadg.config.xml in the config folder holding the mod's configuration * print remaining compiling shaders only if EnableShaderCompilationNotifications is true * the DadgConfigWatcher class watches for changes in the dadg.config.xml file * changes are loaded in the DadgConfig object fetchable from the DadgConfigWatcher object * refactor: move folder paths to the FolderLocator helper class * refactor: move the ability to print messages to InfoPrinter helper class * refactor: the logger config file is now read from the root config folder

---
+++ src/DellarteDellaGuerra/Utils/InfoPrinter.cs
@@ -0,0 +1,24 @@
+﻿using TaleWorlds.Library;
+
+namespace DellarteDellaGuerra.Utils;
+
+/**
+ * <summary>
+ *  A helper class for printing information to the game.
+ * </summary>
+ */
+public static class InfoPrinter
+{
+    /**
+     * <summary>
+     *  Displays a message in the game.
+     * </summary>
+     * <param name="message">
+     *  The message to display.
+     * </param>
+     */
+    public static void Display(string message)
+    {
+        InformationManager.DisplayMessage(new InformationMessage(message, new Color(134, 114, 250)));
+    }
+}
