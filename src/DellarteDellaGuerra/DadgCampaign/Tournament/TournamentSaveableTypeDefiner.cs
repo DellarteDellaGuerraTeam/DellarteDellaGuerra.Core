011e3b33a7f75c5dfe6802908d54fcc89a129548
JoeFwd <jforward.dev@gmail.com>
2024-06-09 15:38:37 +0200

    bug: add new tourney type as saveable type

---
+++ src/DellarteDellaGuerra/DadgCampaign/Tournament/TournamentSaveableTypeDefiner.cs
@@ -0,0 +1,17 @@
+﻿using TaleWorlds.SaveSystem;
+
+namespace DellarteDellaGuerra.DadgCampaign.Tournament
+{
+    public class TournamentSaveableTypeDefiner : SaveableTypeDefiner
+    {
+        public TournamentSaveableTypeDefiner()
+            : base(67705654)
+        {
+        }
+
+        protected override void DefineClassTypes()
+	    {
+		    AddClassDefinition(typeof(TourneyTournament), 3);
+	    }
+    }
+}
