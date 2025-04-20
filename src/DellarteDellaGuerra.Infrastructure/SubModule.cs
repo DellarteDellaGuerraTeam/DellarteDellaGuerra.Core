4ddcb4b08f25003cfcc07ca143e03aae0113c802
JoeFwd <jforward.dev@gmail.com>
2025-04-20 18:09:24 +0200

    temporarily disable reload animation logic until stable

--- src/DellarteDellaGuerra.Infrastructure/SubModule.cs
+++ src/DellarteDellaGuerra.Infrastructure/SubModule.cs
@@ -8,7 +8,6 @@ using DellarteDellaGuerra.Domain.Common.Logging.Port;
 using DellarteDellaGuerra.Domain.DisplayCompilingShaders;
 using DellarteDellaGuerra.Firearm;
 using DellarteDellaGuerra.Firearm.Patches;
-using DellarteDellaGuerra.Firearms;
 using DellarteDellaGuerra.Infrastructure.Configuration.Providers;
 using DellarteDellaGuerra.Infrastructure.DisplayCompilingShaders.Providers;
 using DellarteDellaGuerra.Infrastructure.ExpandedTemplateApi.Logging;
@@ -88,7 +87,7 @@ namespace DellarteDellaGuerra.Infrastructure
         public override void OnMissionBehaviorInitialize(Mission mission)
         {
             base.OnMissionBehaviorInitialize(mission);
-            mission.AddMissionBehavior(new FirearmReloadMissionLogic());
+            // mission.AddMissionBehavior(new FirearmReloadMissionLogic());
         }
 
         public override void RegisterSubModuleObjects(bool isSavedCmapaign)
