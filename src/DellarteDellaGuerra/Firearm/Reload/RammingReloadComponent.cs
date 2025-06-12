3d8c057e928f59d4d6d06ccb0e9fc66e6edb47b2
JoeFwd <jforward.dev@gmail.com>
2025-06-13 01:21:06 +0200

    feat: add first implementation for weapon reload in ramming phase

---
+++ src/DellarteDellaGuerra/Firearm/Reload/RammingReloadComponent.cs
@@ -0,0 +1,72 @@
+﻿using Force.DeepCloner;
+using TaleWorlds.Core;
+using TaleWorlds.Library;
+using TaleWorlds.MountAndBlade;
+
+namespace DellarteDellaGuerra.Firearm.Reload
+{
+    public class RammingReloadComponent : IReloadPhase
+    {
+        private const float ProgressStart = 0.422f;
+        private const float ProgressEnd = 1f;
+
+        private float _progress;
+
+        private readonly WeaponReloadPhaseComponent _reloadPhase;
+
+        public RammingReloadComponent(Agent agent)
+        {
+            _reloadPhase = new WeaponReloadPhaseComponent(() =>
+                new BoneAttachedWeapon(agent, Agent.HandIndex.MainHand, HumanBone.HandL, TransformWeaponFrame));
+        }
+
+        public void OnTick(float dt)
+        {
+            _reloadPhase.OnTick(dt);
+        }
+
+        public void OnReloadStart()
+        {
+            _reloadPhase.OnReloadStart();
+        }
+
+        public void OnReloadProgress(float progress)
+        {
+            _reloadPhase.OnReloadProgress(progress);
+            _progress = progress;
+        }
+
+        public void OnReloadEnd()
+        {
+            _reloadPhase.OnReloadEnd();
+        }
+
+        public float ReloadingProgressStart => ProgressStart;
+        public float ReloadingProgressEnd => ProgressEnd;
+
+        private MatrixFrame TransformWeaponFrame(MatrixFrame weaponFrame)
+        {
+            var newWeaponFrame = weaponFrame.DeepClone();
+            newWeaponFrame.rotation.RotateAboutSide(MathF.PI / 2);
+            newWeaponFrame.rotation.RotateAboutForward(MathF.PI);
+
+            newWeaponFrame.rotation.RotateAboutUp(-0.2f);
+            newWeaponFrame.rotation.RotateAboutForward(-0.2f);
+
+            newWeaponFrame.Advance(0.03f);
+            newWeaponFrame.Elevate(-0.01f);
+
+            newWeaponFrame.Strafe(0.5f);
+
+            // newWeaponFrame.rotation.RotateAboutUp(0.5f);
+            // newWeaponFrame.rotation.RotateAboutSide(0.5f);
+            // newWeaponFrame.rotation.RotateAboutForward(0.5f);
+
+            // newWeaponFrame.rotation.RotateAboutUp(0.1f);
+            // newWeaponFrame.Strafe(0.62f);
+            // newWeaponFrame.Elevate(-0.1f);
+
+            return newWeaponFrame;
+        }
+    }
+}
