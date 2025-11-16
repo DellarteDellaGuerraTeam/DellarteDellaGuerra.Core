cb231c357167b28cde76daeb069671caaa7eeca6
JoeFwd <jforward.dev@gmail.com>
2025-11-16 14:37:15 +0100

    fix behaviour applying cloth simulation to entities with heraldry textures

---
+++ src/DellarteDellaGuerra/Scene/ClothSimulationActivatorMissionLogic.cs
@@ -0,0 +1,19 @@
+﻿using TaleWorlds.MountAndBlade;
+
+namespace DellarteDellaGuerra.Scene
+{
+    /// <summary>
+    /// Needed to prevent the disabling of cloth simulation when heraldry texture is applied 
+    /// to bd_banner_b tagged entities
+    /// Unsure if this is still required for v1.3
+    /// </summary>
+    public class ClothSimulationActivatorMissionLogic : MissionLogic
+    {
+        public override void AfterStart()
+        {
+            base.AfterStart();
+
+            Mission.Current.Scene.SetClothSimulationState(true);
+        }
+    }
+}
