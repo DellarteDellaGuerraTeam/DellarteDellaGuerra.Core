843af4d33ddececb2c1cbf31bfca48db1c7b9d3e
JoeFwd <jforward.dev@gmail.com>
2026-03-14 23:33:02 +0100

    fix(cannons): stop AI from using invisible cannons during sieges

--- src/DellarteDellaGuerra.Integration/SiegeEngines/Mission/Battle/CannonTeamMissionLogic.cs
+++ src/DellarteDellaGuerra.Integration/SiegeEngines/Mission/Battle/CannonTeamMissionLogic.cs
@@ -7,27 +7,33 @@ namespace DellarteDellaGuerra.Integration.SiegeEngines.Mission.Battle;
 
 public class CannonTeamMissionLogic : MissionLogic
 {
-    public override void OnAddTeam(TaleWorlds.MountAndBlade.Team team)
+    // Triggered on the Deployment -> Battle transition, which happens exactly once when the
+    // deployment phase ends. This guarantees that we add the team and forced use flag to deployed cannons.
+    // Otherwise, the AI will try to use invisible cannons.
+    public override void OnMissionModeChange(MissionMode oldMissionMode, bool atStart)
     {
-        base.OnAddTeam(team);
+        base.OnMissionModeChange(oldMissionMode, atStart);
+
+        if (oldMissionMode != MissionMode.Deployment || Mission.Mode != MissionMode.Battle)
+            return;
 
         TaleWorlds.MountAndBlade.Mission.Current.ActiveMissionObjects
             .OfType<GenericCannon>()
-            .Where(script => script.Side.Equals(BattleSideEnum.Attacker) && team.IsAttacker)
+            .Where(script => script.Side.Equals(BattleSideEnum.Attacker))
             .ToList()
             .ForEach(script =>
             {
-                script.Team = team;
+                script.Team = Mission.Teams.Attacker;
                 script.SetForcedUse(true);
             });
 
         TaleWorlds.MountAndBlade.Mission.Current.ActiveMissionObjects
             .OfType<GenericCannon>()
-            .Where(script => script.Side.Equals(BattleSideEnum.Defender) && team.IsDefender)
+            .Where(script => script.Side.Equals(BattleSideEnum.Defender))
             .ToList()
             .ForEach(script =>
             {
-                script.Team = team;
+                script.Team = Mission.Teams.Defender;
                 script.SetForcedUse(true);
             });
     }
